117dd15996d45663062bac7b7cc97f6696d8cb84
Bryan Lincoln <bryanufg@gmail.com>
2024-06-21 12:23:22 -0300

    feat: initial commit

---
+++ workloads/1.to_run/example.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# {"requirements": {"ram": 8, "vram": 12, "ramp_up_time": 30}}
+
+./workloads/launch.sh echo "Hello, World!"
---
+++ workloads/launch.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# This is an example script that implements the base command for all workloads
+# Run the Docker container with the provided argument
+docker run --rm alpine "$@"
