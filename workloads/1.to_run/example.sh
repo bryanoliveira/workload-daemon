81749bbdf144b831d826fa53ce5b8e7b50e3a824
Bryan Lincoln <bryanufg@gmail.com>
2024-06-22 10:21:53 -0300

    fix: update example workload

--- workloads/1.to_run/example.sh
+++ workloads/1.to_run/example.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 # {"requirements": {"ram": 8, "vram": 12, "ramp_up_time": 30}}
 
+current_file=$(basename "${BASH_SOURCE[0]}" .sh)
+echo "Current workload: $current_file"
 ./workloads/launch.sh echo "Hello, World!"
