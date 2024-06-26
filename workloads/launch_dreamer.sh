f9d2bf752e4b10d5fc45993b100ba0a78940c0e1
Bryan Lincoln <bryanufg@gmail.com>
2024-06-26 18:21:15 -0300

    fix: update dreamer launcher

--- workloads/launch_dreamer.sh
+++ workloads/launch_dreamer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
-container_name="aluno_bryan-$1"
+experiment="$1"
 shift
+container_name="aluno_bryan-$experiment"
 
 # Run the Docker container with the provided arguments
 CONTAINER_ID=$(docker run -d --gpus '"device=2"' --cpus=16.0 --memory=16g \
@@ -9,6 +9,8 @@ CONTAINER_ID=$(docker run -d --gpus '"device=2"' --cpus=16.0 --memory=16g \
     -v /raid/bryan/dreamerv3/logdir:/app/logdir \
     --name "$container_name" \
     aluno_bryan-sldp_dreamerv3 \
+    python dreamerv3/main.py \
+    --logdir ./logdir/$(date "+%Y%m%d_%H%M%S")-$experiment \
     "$@")
 
 # The container is runnig detached, but we block this thread until finished
